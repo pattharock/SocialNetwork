86ba2eea8bd0066ff23f1e56a6db43cf9221f88f
pattharock <ritvik1609#gmail.com>
2021-05-20 20:46:31 +0800

    CommentCreate and CommentList components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PostCreate from "./PostCreate.js";
+import PostList from "./PostList.js";
 
 const App = () => {
   return (
     <div className='container'>
       <h1>Create Posts</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
