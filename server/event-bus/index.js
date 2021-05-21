4a620cc95c9ca5bd7d54ce8328376b28900f4644
pattharock <ritvik1609#gmail.com>
2021-05-21 09:42:34 +0800

    event-bus routes setup

--- server/event-bus/index.js
+++ server/event-bus/index.js
@@ -4,3 +4,26 @@ const axios = rerquire("axios");
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events").catch((err) => {
+    console.log("Error in reaching post service");
+    console.log(err);
+  });
+
+  axios.post("http://localhost:4001/events").catch((err) => {
+    console.log("Error in reaching comment service");
+    console.log(err);
+  });
+
+  axios.post("http://localhost:4002/events").catch((err) => {
+    console.log("Error in reaching query service");
+    console.log(err);
+  });
+
+  app.listen(4005, () => {
+    console.log("Listening on 4005");
+  });
+});
