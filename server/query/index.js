f5c6b0001967ff5f2fdc02f5b42f26d9ac1b68e5
pattharock <ritvik1609#gmail.com>
2021-05-21 16:10:04 +0800

    query service routes setup

--- server/query/index.js
+++ server/query/index.js
@@ -1,6 +1,39 @@
 const express = require("express");
-const cors = requirr("express");
+const cors = require("express");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = {
+      id,
+      title,
+      comments: [],
+    };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = req.body;
+    posts[postId].comments.push({
+      id,
+      content,
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
