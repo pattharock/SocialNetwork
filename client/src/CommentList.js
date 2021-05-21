c5ac89c0e2d65dd9cc0ad5b0d5de02a907520bbb
pattharock <ritvik1609@gmail.com>
2021-05-21 19:35:27 +0800

    frontend refactor for comment moderation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content = "";
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderaiton";
+    }
+    if (comment.status === "accepted") {
+      content = comment.content;
+    }
+    if (comment.status === "rejected") {
+      content = "This comment is reject due to violation of Public Policy";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return (
